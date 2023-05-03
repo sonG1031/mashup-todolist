1440ceb88cc03c8e3c200c4d568ae8fa2344e373
Minsub Song <minsub1031@gmail.com>
2023-05-03 09:48:57 +0900

    2023-05-03

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplete from './components/TodoTemplete';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef
+  }
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle/>
+      <TodoTemplete>
+        <TodoHead/>
+        <TodoList/>
+      </TodoTemplete>
+    </>
   );
 }
 
---
+++ src/components/TodoHead.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+    padding-top: 48px;
+    padding-left: 32px;
+    padding-right: 32px;
+    padding-bottom: 24px;
+    border-bottom: 1px solid #e9ecef;
+    h1 {
+        margin: 0;
+        font-size: 36px;
+        color: #343a40;
+    }
+
+    .day {
+        margin-top: 4px;
+        font-size: 36px;
+        color: #868e96;
+    }
+
+    .task-left {
+        margin-top: 40px;
+        font-size: 18px;
+        color: #20c997;
+        font-weight: bold;
+    }
+`
+
+function TodoHead() {
+    return (
+        <TodoHeadBlock>
+            <h1>2023년 5월 3일</h1>
+            <div className='day'>수요일</div>
+            <div className='task-left'>할일 2개 남음</div>
+        </TodoHeadBlock>
+    )
+}
+
+export default TodoHead;
